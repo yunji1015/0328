364bfa1fb7fad6ee7a114e578270902957295fac
30912 <129143168+yunji1015@users.noreply.github.com>
2023-03-28 05:08:31 +0000

    or.c추가

---
+++ .ccls-cache/@home@runner@0328/or.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+main()
+{
+  int a=15;
+  int b=20;
+  int c = a|b;
+
+  printf("or 연산결과 : %d", c);
+}
--- main.c
+++ main.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-main()
-{
-  int a=15;
-  int b=20;
-  int c = a&b;
+// #include <stdio.h>
+// main()
+// {
+//   int a=15;
+//   int b=20;
+//   int c = a&b;
 
-  printf("AND 연산결과 : %d", c);
-}
+//   printf("AND 연산결과 : %d", c);
+// }
